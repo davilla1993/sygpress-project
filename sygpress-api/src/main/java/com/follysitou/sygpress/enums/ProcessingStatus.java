14248ef87c3afb85642bd91ba8645ffbfdedc4f9
carlo <carlogbossou93@gmail.com>
2025-11-18 12:38:33 +0000

    Ajout du statut COLLECTE dans ProcessingStatus

--- sygpress-api/src/main/java/com/follysitou/sygpress/enums/ProcessingStatus.java
+++ sygpress-api/src/main/java/com/follysitou/sygpress/enums/ProcessingStatus.java
@@ -1,6 +1,7 @@
 package com.follysitou.sygpress.enums;
 
 public enum ProcessingStatus {
+    COLLECTE,
     EN_LAVAGE,
     EN_REPASSAGE,
     PRET,
--- sygpress-api/src/main/java/com/follysitou/sygpress/repository/CategoryRepository.java
+++ sygpress-api/src/main/java/com/follysitou/sygpress/repository/CategoryRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
     Optional<Category> findByPublicId(String publicId);
     Optional<Category> findByName(String name);
     boolean existsByName(String name);
